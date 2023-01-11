ecb030f533e6d976611b44b9173056346ba86900
EmiliaMlak <emiliamlak@gmail.com>
2023-01-11 19:08:16 +0000

    create PageTitle files, skeleton of component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Button from './components/Button/Button';
+import PageTitle from './components/PageTitle/PageTitle';
 
 function App() {
   return (
     <div>
+      <PageTitle>SETS</PageTitle>
       <Button onClick={() => console.log('HI!')}>Default BUTTON</Button>
       <Button type="secondary" onClick={() => console.log('HI!')}>White BUTTON</Button>
     </div>
